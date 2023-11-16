72c195a2d4a452501bc5fa29e2dfcfd662d2c8f5
Prosperugonna <prosperugonna@gmail.com>
2023-11-16 09:17:56 +0000

    ok

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""flask app for suthentication"""
+from flask import Flask, jsonify, request, redirect, abort
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/')
+def index():
+    """Get the index page"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    '''
+    return all users
+    '''
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    '''
+    login user
+    '''
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    cookies = AUTH.create_session(email)
+    resp = jsonify({"email": email, "message": "logged in"})
+    resp.set_cookie("session_id", cookies)
+    return resp
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    '''
+    log out user
+    '''
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        Auth.destroy_session(user.id)
+        return redirect("/")
+    abort(403)
+
+
+@app.route("/profile", method=["GET"], strict_slashes=False)
+def profile() -> str:
+    """
+    get profile
+    Return:
+        message if found
+    else:
+        abort
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
+
+@app.route("/reset_password", method=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    get reset password token
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    '''
+    update password endpoint
+    '''
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        res = AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(400)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""DB module for the code
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+from user import Base
+from user import User
+
+
+class DB:
+    """DB class for db"""
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password):
+        """add user with email"""
+        new_user = User()
+        new_user.email = email
+        new_user.hashed_password = hashed_password
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs):
+        """Find a user in user"""
+        users = self._session.query(User)
+        for k, v in kwargs.items():
+            if k not in User.__dict__:
+                raise InvalidRequestError
+            for user in users:
+                if getattr(user, k) == v:
+                    return user
+        raise NoResultFound
+
+    def update_user(self, user_id, **kwargs):
+        """Update a user in user"""
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if k not in User.__dict__:
+                raise ValueError
+            user.k = v
+        self._session.commit()
